9dc040956b1aa67130343b9d766beef6c7c2da41
HYUNSOOSHIN <shs0655@gmail.com>
2020-11-08 21:24:19 +0900

    [20/11/08] add - images, plan folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
+import zior from './../images/zior.jpg'
+
 function App() {
   return (
     <div className="App">
+      <img style={{}} src={zior} alt={'main img'} />
       <p>프로젝트 시작해보자</p>
     </div>
   );
