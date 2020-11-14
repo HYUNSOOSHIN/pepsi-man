5c8ff46b977100723c3cb821c73e81aad43a1b88
HYUNSOOSHIN <shs0655@gmail.com>
2020-11-14 18:26:01 +0900

    [20/11/14] add - darkMode toggle switch button

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react"
+import { darkColors, whiteColors } from "../../util"
 import Header from "./Header"
 import Footer from "./Footer"
 
+import { Switch } from "@material-ui/core"
+import { withStyles } from "@material-ui/core/styles"
+
 const Layout = (props) => {
   const { children } = props
+  const [toggle, setToggle] = useState(localStorage.getItem("darkMode") === "true")
+
+  const setColorMode = (colorMode) => {
+    for (const [key, value] of Object.entries(colorMode)) {
+      document.documentElement.style.setProperty(`--${key}`, `${value}`)
+    }
+  }
+
+  setColorMode(localStorage.getItem("darkMode") === "true" ? darkColors : whiteColors)
 
   return (
     <div id={"container"}>
       <div id={"toggle_view"}>
-        <p>다크모드</p>토글버튼
+        <p>다크모드</p>
+        <AntSwitch
+          checked={toggle}
+          onChange={() => {
+            setColorMode(toggle ? whiteColors : darkColors)
+            setToggle(!toggle)
+            localStorage.setItem("darkMode", `${!toggle}`)
+          }}
+          name="color_toggle"
+        />
       </div>
       <Header />
       <div id={"page"}>{children}</div>
@@ -17,3 +40,37 @@ const Layout = (props) => {
 }
 
 export default Layout
+
+const AntSwitch = withStyles((theme) => ({
+  root: {
+    overflow: "visible",
+    width: 28,
+    height: 16,
+    padding: 0,
+  },
+  switchBase: {
+    padding: "3px 2px",
+    color: theme.palette.grey[500],
+    "&$checked": {
+      transform: "translateX(12px)",
+      color: theme.palette.common.white,
+      "& + $track": {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: "none",
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white,
+  },
+  checked: {},
+}))(Switch)
---
+++ src/util.js
@@ -0,0 +1,11 @@
+export const darkColors = {
+  background: "#1b1d21",
+  text: "#b6b7b8",
+  border: "#646568",
+}
+
+export const whiteColors = {
+  background: "#ffffff",
+  text: "#232323",
+  border: "#393c42",
+}
