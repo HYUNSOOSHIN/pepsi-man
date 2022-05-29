d59c28d3d6e3d05cb8005345b96ee213f6bee35c
HYUNSOOSHIN <shs0655@gmail.com>
2022-05-29 15:35:00 +0900

    [22/05/29] Admin - Apply the Firebase function to the talks page

--- src/components/page/admin/talks/index.jsx
+++ src/components/page/admin/talks/index.jsx
@@ -1,8 +1,79 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import styled from "styled-components"
+import { dbService } from "../../../../fireBase"
 import Layout from "components/common/Layout"
 
 const AdminTalks = () => {
-  return <Layout></Layout>
+  const [talkList, setTalkList] = useState([])
+
+  useEffect(() => {
+    dbService.collection("talks").onSnapshot((snapshot) => {
+      setTalkList(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    })
+  }, [])
+
+  const onClickTalkDelete = (id) => {
+    if (confirm("Really?")) {
+      dbService.collection("talks").doc(id).delete()
+      dbService
+        .collection("comments")
+        .where("talkSeq", "==", id)
+        .get()
+        .then((docs) => docs.forEach((doc) => doc.ref.delete()))
+    }
+  }
+
+  return (
+    <Layout>
+      <Table>
+        <colgroup>
+          <col style={{}}></col>
+          <col style={{ width: "30%" }}></col>
+          <col style={{ width: "60%" }}></col>
+          <col style={{}}></col>
+        </colgroup>
+        <thead>
+          <tr>
+            <th>NO.</th>
+            <th>TITLE</th>
+            <th>CONTENTS</th>
+            <th>DEL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {talkList
+            .sort((a, b) => a.createdAt - b.createdAt)
+            .map((i, idx) => (
+              <tr key={idx}>
+                <td style={{ textAlign: "center" }}>{idx + 1}</td>
+                <td>{i.title}</td>
+                <td>{i.contents}</td>
+                <td style={{ textAlign: "center" }}>
+                  <button onClick={() => onClickTalkDelete(i.id)}>DEL</button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </Layout>
+  )
 }
 
 export default AdminTalks
+
+const Table = styled.table`
+  width: 1024px;
+  margin: 0 auto;
+  border: 1px solid;
+  th {
+    padding: 5px;
+    border: 1px solid;
+  }
+  td {
+    padding: 5px;
+    border: 1px solid;
+  }
+  @media (max-width: 1024px) {
+    width: 90%;
+  }
+`
